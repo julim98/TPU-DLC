f47c4da24424922c873cd3043457851ad671ac37
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2022-03-31 04:21:22 -0300

    31/03/22 04:21

--- src/main/java/main/Indexar.java
+++ src/main/java/main/Indexar.java
@@ -3,29 +3,27 @@ package main;
 import clases.Documento;
 import clases.Posteo;
 import clases.Vocabulario;
+import soporte.Indexador;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.Console;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.util.*;
 
 public class Indexar {
 
-    private static HashSet<Documento> documentos = new HashSet<>();
-    private static Hashtable<Integer,Vocabulario> vocabularioAux;
-    private static Hashtable<Integer,Vocabulario> vocabulario = new Hashtable<>();
-    private static HashSet<Posteo> posteos = new HashSet<>();
-
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
-        indexar();
+        Indexador indexador = new Indexador();
+        indexador.indexar(directoryPath());
 
         while (true) {
             System.out.println("Opciones\n");
-            System.out.println("1 - Mostrar vocabulario");
+            System.out.println("1 - Mostrar Vocabulario");
             System.out.println("2 - Mostrar Documentos");
             System.out.println("3 - Mostrar Posteos");
             System.out.println("4 - Salir");
@@ -37,18 +35,15 @@ public class Indexar {
             switch (opcion) {
                 case 1:
                     System.out.println("Vocabulario: \n");
-                    vocabulario.forEach((integer, terminos) -> System.out.println());
-                    System.out.println("Cantidad de Terminos: " + vocabulario.size());
+                    indexador.getVocabulario().forEach((integer, termino) -> System.out.println(termino));
                     break;
                 case 2:
                     System.out.println("Documentos: \n");
-                    documentos.forEach(System.out::println);
-                    System.out.println("Cantidad de documentos: " + documentos.size());
+                    indexador.getDocumentos().forEach(System.out::println);
                     break;
                 case 3:
                     System.out.println("Posteos: \n");
-                    posteos.forEach(System.out::println);
-                    System.out.println("Cantidad de Posteos: " + posteos.size());
+                    indexador.getPosteos().forEach(System.out::println);
                     break;
                 default:
                     return;
@@ -56,74 +51,14 @@ public class Indexar {
         }
     }
 
-    public static void indexar() throws FileNotFoundException {
-
-        Scanner docScan;
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        fileChooser.showOpenDialog(fileChooser);
-        String ruta = fileChooser.getCurrentDirectory().getPath();
-
-        File carpeta = new File(ruta);
-
-        String palabra;
-
-        System.out.println("Comenzando la indexacion de lo documentos\n" +
-                "Por favor espere mientras se completa");
-
-        int i = 0;
-        //Recorre un solo doc
-        for (File docFile: Objects.requireNonNull(carpeta.listFiles())) {
-
-            vocabularioAux = new Hashtable<>();
-            Documento documento = new Documento(docFile.getPath(), docFile.getName());
-            documentos.add(documento);
-            docScan = new Scanner(docFile);
-
-            //System.out.println("Comenzando lectuda del ducumento n°: "+ i + " " + docFile.getName());
-            i++;
-            while (docScan.hasNext()) {
-
-                palabra = docScan.next();
-                int key = palabra.hashCode();
-
-
-                if (vocabularioAux.containsKey(key)) {
-                    vocabularioAux.get(key).increaseMaxFrec();
-                } else {
-                    Vocabulario newTermino = new Vocabulario(palabra);
-                    vocabularioAux.put(key, newTermino);
-                    posteos.add(new Posteo(newTermino, documento));
-                }
-            }
-
-
-
-            if (!vocabulario.isEmpty())
-            {
-                int maxFrecPalabra;
-                int maxFrecPalabraAux;
-
-                for (Vocabulario terminoAux: vocabularioAux.values()) {
-                    Vocabulario termino = vocabulario.get(terminoAux.getPalabra().hashCode());
-                    if (termino != null)
-                    {
-                        termino.increaseCantDoc();
-
-                        maxFrecPalabra = termino.getMaxFrecuenciaPalabra();
-                        maxFrecPalabraAux = terminoAux.getMaxFrecuenciaPalabra();
-                        if (maxFrecPalabra < maxFrecPalabraAux)
-                        {
-                            termino.setMaxFrecuenciaPalabra(maxFrecPalabraAux);
-                        }
-                    }
-                    else {
-                        vocabulario.put(terminoAux.getPalabra().hashCode(), terminoAux);
-                    }
-                }
-            }else {
-                vocabulario = new Hashtable<>(vocabularioAux);
-            }
-        }
+    /**
+     * Abre una ventana donde se solicita que se seleccione una carpeta
+     * @return devuelve la ruta del directorio seleccionado
+     */
+    public static String directoryPath(){
+        JFileChooser directoryChooser = new JFileChooser();
+        directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        directoryChooser.showOpenDialog(directoryChooser);
+        return String.valueOf(directoryChooser.getSelectedFile());
     }
 }
--- src/main/java/soporte/Indexador.java
+++ src/main/java/soporte/Indexador.java
@@ -1,4 +1,105 @@
 package soporte;
 
+import clases.Documento;
+import clases.Posteo;
+import clases.Vocabulario;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Indexador {
+
+    private static HashSet<Documento> documentos = new HashSet<>();
+    private static Hashtable<Integer,Vocabulario> vocabularioAux;
+    private static Hashtable<Integer,Vocabulario> vocabulario = new Hashtable<>();
+    private static HashSet<Posteo> posteos = new HashSet<>();
+
+    /**
+     * Comienza la indexacion de los archivos
+     * @throws FileNotFoundException
+     */
+    public void indexar(String ruta) throws FileNotFoundException {
+
+        Scanner docScan;
+        File carpeta = new File(ruta);
+
+        String palabra;
+        System.out.println("Comenzando la indexacion de lo documentos\n" +
+                "Por favor espere mientras se completa");
+
+        int i = 0;
+
+        //Recorre cada documento txt de la carpeta
+        for (File docFile: Objects.requireNonNull(carpeta.listFiles((File pathname) -> pathname.getName().endsWith(".txt")))) {
+
+            vocabularioAux = new Hashtable<>();
+            Documento documento = new Documento(docFile.getPath(), docFile.getName());
+            documentos.add(documento);
+            docScan = new Scanner(docFile);
+
+            //System.out.println("Comenzando lectuda del ducumento n°: "+ i + " " + docFile.getName());
+            i++;
+
+            //Recorre cada palabra del documento
+            while (docScan.hasNext()) {
+
+                palabra = docScan.next();
+                int key = palabra.hashCode();
+
+                //Pregunta si la palabra ya se encuentra y si lo esta aumenta su frecuencia y si no la agrega
+                if (vocabularioAux.containsKey(key)) {
+                    vocabularioAux.get(key).increaseMaxFrec();
+                } else {
+                    Vocabulario newTermino = new Vocabulario(palabra);
+                    vocabularioAux.put(key, newTermino);
+                    posteos.add(new Posteo(newTermino, documento));
+                }
+            }
+
+            //Aca se controla si los terminos indexados en el documento ya se encontraban en el vocabulario y si lo
+            // estaban se guada el de mayor frecuencia
+            if (!vocabulario.isEmpty())
+            {
+                int maxFrecPalabra;
+                int maxFrecPalabraAux;
+
+                for (Vocabulario terminoAux: vocabularioAux.values()) {
+                    Vocabulario termino = vocabulario.get(terminoAux.getPalabra().hashCode());
+                    if (termino != null)
+                    {
+                        termino.increaseCantDoc();
+
+                        maxFrecPalabra = termino.getMaxFrecuenciaPalabra();
+                        maxFrecPalabraAux = terminoAux.getMaxFrecuenciaPalabra();
+                        if (maxFrecPalabra < maxFrecPalabraAux)
+                        {
+                            termino.setMaxFrecuenciaPalabra(maxFrecPalabraAux);
+                        }
+                    }
+                    else {
+                        vocabulario.put(terminoAux.getPalabra().hashCode(), terminoAux);
+                    }
+                }
+            }else {
+                vocabulario = new Hashtable<>(vocabularioAux);
+            }
+        }
+    }
+
+    public HashSet<Documento> getDocumentos() {
+        return documentos;
+    }
+
+    public Hashtable<Integer, Vocabulario> getVocabulario() {
+        return vocabulario;
+    }
+
+    public HashSet<Posteo> getPosteos() {
+        return posteos;
+    }
 }
